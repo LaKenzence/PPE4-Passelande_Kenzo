e3dc86b41de6ebf1f7bb864097f751307043bad3
kpasselande <kenzo.passelande@gmail.com>
2022-05-05 10:13:09 +0200

    3.5 Fini

---
+++ app/src/main/java/com/example/ppe4_passelande_kenzo/Map.java
@@ -0,0 +1,14 @@
+package com.example.ppe4_passelande_kenzo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
